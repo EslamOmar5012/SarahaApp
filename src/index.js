9edf96619da5e2edfdb461bbcbdbfb41de5c3f41
Eslam Omar <eslam.O.elkhabery@gmail.com>
2026-04-03 14:37:17 +0200

    refactor: remove unused encryption function and related code from index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import bootstrap from "./app.bootstrap.js";
-import { encrypt } from "./common/index.js";
 
 await bootstrap();
-
-const text1 = await encrypt("Eslam Omar");
-
-// console.log(text1);
